9f3cca8cd34df7d332e9c0000228a99113b70e29
gilishechter <gishecht@g.jct.ac.il>
2024-01-09 20:29:31 +0200

    add stage 0

---
+++ stage0/Program7795.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stage0
+{
+    partial class Program7795
+    {
+        static void Welcome7795()
+        {
+            Console.WriteLine("I'm also here!!");
+        }
+    }
+}
---
+++ stage0/Program8937.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome8937();
+            welcome7795();
+            Console.ReadKey();
+
+        }
+        static partial void welcome7795();
+
+        private static void welcome8937()
+        {
+            Console.WriteLine("Enter yoyr name:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0},welcome to my first console application", userName);
+        }
+    }
+
+}
