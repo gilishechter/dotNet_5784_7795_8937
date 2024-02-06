d4243b15cbe5677fe32572a2ef21e59459db18bc
gilishechter <gishecht@g.jct.ac.il>
2024-02-06 16:23:05 +0200

    TRY

---
+++ BL/BlApi/IBlDateProject.cs
@@ -0,0 +1,11 @@
+﻿using BO;
+
+namespace BlApi;
+
+internal interface IBlDateProject
+{
+    public void createStartProjectDate();
+    public void createEndProjectDate();
+    public DateTime? EndProjectDate();
+    public DateTime? startProjectDate();
+}
